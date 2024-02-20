7e1c1effe0ecf667917c10049b19284bac0a1c2c
Eva Ramos <ecolemanm13@gmail.com>
2024-02-20 12:39:30 -0600

    adding "computer" to the output log

--- rps.js
+++ rps.js
@@ -1,10 +1,10 @@
 const computerSelection = Math.floor(Math.random() * 3)
     if (computerSelection < 1) 
-       console.log("Rock");
+       console.log("Computer: Rock");
     else if (computerSelection < 2) 
-        console.log("Paper");
+        console.log("Computer: Paper");
     else 
-       console.log("Scissors");
+       console.log("Computer: Scissors");
 const playerSelection = "rock";
 console.log("I chose " + playerSelection);
 
