ab9eb302196f832e8edf4e1304cdd90be156678f
Eva Ramos <ecolemanm13@gmail.com>
2024-02-20 13:11:00 -0600

    got the function getComputerChoice to work

--- rps.js
+++ rps.js
@@ -1,10 +1,17 @@
-const computerSelection = Math.floor(Math.random() * 3)
+function getComputerChoice() {
+    const computerSelection = Math.floor(Math.random() * 3)
     if (computerSelection < 1) 
        console.log("Computer: Rock");
     else if (computerSelection < 2) 
         console.log("Computer: Paper");
     else 
        console.log("Computer: Scissors");
-const playerSelection = "rock";
-console.log("I chose " + playerSelection);
+};
+getComputerChoice()
 
+
+
+
+//function playerSelection () {
+    const playerSelection = "rock";
+    console.log("I chose " + playerSelection);
