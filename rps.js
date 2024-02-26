1f7be4720e2dc7aed25f05987250a79e84117de8
Eva Ramos <ecolemanm13@gmail.com>
2024-02-26 14:27:18 -0600

    trying the example from youtube

--- rps.js
+++ rps.js
@@ -1,24 +1,34 @@
-function getComputerChoice() {
-    let choice = ['Rock', 'Paper', 'Scissors']
-    let computerSelection = choice[Math.floor(Math.random() * choice.length)]
-    console.log("computer chose " + computerSelection)
-};
-function getPlayerSelection () {
-   const playerSelection = prompt("Choose rock, paper, scissors")
-    if (playerSelection === 'paper' || playerSelection === 'rock' || playerSelection === 'scissors')
-        console.log("you chose " + playerSelection)
-    else 
-        console.log("error")
-   console.log(playerSelection)
-};
-getComputerChoice();
-getPlayerSelection();
-function playRound(computerSelection, playerSelection) {
-    if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper')
-        console.log("you lose")
-    else if (playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors')
-        console.log("you win")
-    else 
-        console.log("tie")
+//function getComputerChoice() {
+let choice = ['Rock', 'Paper', 'Scissors']
+
+function play() {
+    playRound();
 }
-playRound();
+function playRound() {
+    const playerSelection = getPlayerSelection()
+    const computerSelection = getComputerSelection()
+}
+function getPlayerSelection () {
+    const input = prompt("Choose rock, paper, scissors")
+    //if (playerSelection === 'paper' || playerSelection === 'rock' || playerSelection === 'scissors')
+      //  console.log("you chose " + playerSelection)
+    //else 
+      //  console.log("error")
+   console.log(input)
+};
+function getComputerSelection(){
+    const cinput = choice[Math.floor(Math.random() * choice.length)]
+    console.log("computer chose " + cinput)
+};
+
+//getComputerChoice();
+//getPlayerSelection();
+
+    //if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper')
+   //     console.log("you lose")
+    //else if (playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors')
+     //   console.log("you win")
+    //else 
+      //  console.log("tie")
+    
+play();
