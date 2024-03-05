f3e88c3bd12f8bb157b2d350b7e5fe4eccd9bdfc
emramos21 <146121941+emramos21@users.noreply.github.com>
2024-03-05 18:20:59 +0000

    getting rid of the console logs

--- rps1.js
+++ rps1.js
@@ -1,5 +1,4 @@
 const choice = ["rock", "paper", "scissors"]
-//const winners = [];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const playerScoreDisplay = document.getElementById("player score");
@@ -9,10 +8,8 @@ let playerScore = 0;
 let computerScore = 0;
 function playGame(playerChoice) {
     computerSelection = choice[Math.floor(Math.random() * choice.length)];
-    console.log("Computer chose " + computerSelection);
     let result = " ";
     if (playerChoice === computerSelection){
-        console.log("tie");
         result = "tie";
     } else {
         switch(playerChoice){
