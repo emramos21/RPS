9dbb52c904a44c0bf5d1258fba0ec73dd4538b90
emramos21 <146121941+emramos21@users.noreply.github.com>
2024-03-05 17:40:04 +0000

    so computer output is working and the buttons are working

--- rps1.js
+++ rps1.js
@@ -1,3 +1,12 @@
 const choice = ["rock", "paper", "scissors"]
 const winners = [];
-const playerSelection = document.getElementById("playerSelection");
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const playerScore = document.getElementById("playerScore");
+const computerScore = document.getElementById("computerScore");
+
+function playGame() {
+    computerSelection = choice[Math.floor(Math.random() * choice.length)];
+    console.log("Computer chose " + computerSelection);
+};
+playGame();
