4b666dc5a465f9e1d9d8c8eebf269a6afbef73f9
emramos21 <146121941+emramos21@users.noreply.github.com>
2024-03-05 17:57:41 +0000

    working on getting the html to show the results

--- rps1.js
+++ rps1.js
@@ -1,12 +1,33 @@
 const choice = ["rock", "paper", "scissors"]
-const winners = [];
+//const winners = [];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
-const playerScore = document.getElementById("playerScore");
-const computerScore = document.getElementById("computerScore");
+//const playerScore = document.getElementById("playerScore");
+const resultDisplay = document.getElementById("result");
 
-function playGame() {
+function playGame(playerChoice) {
     computerSelection = choice[Math.floor(Math.random() * choice.length)];
     console.log("Computer chose " + computerSelection);
-};
+    //console.log("me" + playerDisplay)
+    let result = " ";
+    if (playerChoice === computerSelection){
+        console.log("tie");
+        result = "tie";
+    } else {
+        switch(playerChoice){
+            case "rock":
+                result = (computerSelection === "scissors") ? "you win" : "you lose";
+                break;
+            case "paper":
+                result = (computerSelection === 'rock') ? "you win" : "you lose";
+                break;
+            case "scissors":
+                result = (computerSelection === 'paper') ? "you win" : "you lose";
+                break;
+    }
+}
+playerDisplay.textContent = `player: ${playerChoice}`;
+computerDisplay.textContent = `player: ${computerSelection}`;
+resultDisplay.textContent = `player: ${result}`;
+    }
 playGame();
